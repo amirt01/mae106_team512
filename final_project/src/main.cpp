8375b4a55fa5bd105e925bd240cb8db79f404542
amir tadros <amirtadros01@gmail.com>
2022-05-14 18:06:07 -0700

    Callibration

---
+++ final_project/src/main.cpp
@@ -0,0 +1,85 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <LSM303.h>
+#include <Wire.h>
+
+LSM303 compass;
+LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32768, -32768, -32768};
+
+Servo servo;
+
+int servoPin; //define servo pin attached
+int solenoid; //pin solenoid is attahced to
+int switchPin; //pin that the switch is connected to
+int reedSwitch; // variable storing position of reed switch
+int servoDirection; // direction servo is pointing
+int robotPosition; // stores position of the robot itself
+int solenoidState = LOW;
+int potentOne;
+int potentTwo;
+int pistonMode = 1;
+int DesiredSteeringAngle;
+int TimeBetweenFires;
+int PullUpSwitch;
+
+enum State{
+  Callibration,
+  PositionAssignment,
+  Running,
+  Finish,
+} state;
+
+unsigned long previous_time = 0;
+
+// Time Variables
+unsigned long lastmilli = 0; // time since solenoid opened
+const long interval = 1000; // interval to turn on and off solenoid
+
+
+void setup() {
+  servo.attach(servoPin);
+  pinMode(solenoid, OUTPUT);
+  pinMode(switchPin, INPUT_PULLUP);
+  pinMode(reedSwitch, INPUT_PULLUP);
+  Serial.begin(9600);
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  compass.writeReg(0x24, 0x74);
+
+  previous_time = millis();
+}
+
+void loop() {
+  switch(state) {
+    case Callibration:
+      // Callibrate Compass
+      compass.read();
+      
+      // Update min values
+      running_min.x = min(running_min.x, compass.m.x);
+      running_min.y = min(running_min.y, compass.m.y);
+      running_min.z = min(running_min.z, compass.m.z);
+
+      // Update max values
+      running_max.x = max(running_max.x, compass.m.x);
+      running_max.y = max(running_max.y, compass.m.y);
+      running_max.z = max(running_max.z, compass.m.z);
+    
+      // Continue callibration for 10 seconds and then assign compass min and max
+      if (millis() > previous_time + 10000) {
+        compass.m_min = running_min;
+        compass.m_max = running_max;
+        previous_time = millis();
+      }
+      break;
+    case PositionAssignment:
+      break;
+    case Running:
+      // Running robot
+      break;
+    case Finish:
+      // Finish run
+      break;
+  }
+}
